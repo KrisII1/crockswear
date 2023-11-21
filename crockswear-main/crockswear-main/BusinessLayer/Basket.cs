28cbbe27e71a311c80df853faefb7b87e5e33911
KrisII1 <133354279+KrisII1@users.noreply.github.com>
2023-11-21 21:44:17 +0200

    Add files via upload

---
+++ crockswear-main/crockswear-main/BusinessLayer/Basket.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Basket
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public Shoe Shoe { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        [Range(0,999999)]
+        public decimal Price { get; set; }
+        [Range(0, 999999)]
+        [Required]
+        public decimal Shoeprice { get; set; }
+        public OrderStatus Status { get; set; }
+        public Bill Bill { get; set; }
+
+
+        public Basket    ()
+        {
+
+        }
+
+
+
+        public Basket(int id,User user, Shoe shoe, int quantity, decimal price, decimal shoeprice, OrderStatus status)
+        {
+            this.Id= id;
+            this.Shoe= shoe;
+            this.Quantity= quantity;
+            this.Price= price;
+            this.Shoeprice= shoeprice;
+            this.Status= status;
+               
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ crockswear-main/crockswear-main/DataLayer/CrockDBContext.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLayer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Reflection.Metadata;
+
+namespace DataLayer
+{
+    public class CrockDBContext : DbContext
+    {
+        public CrockDBContext()
+        {
+
+        }
+        public CrockDBContext(DbContextOptions contextOptions) : base(contextOptions)
+        {
+
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder OptionsBuilder)
+        {
+            if (!OptionsBuilder.IsConfigured)
+            {
+                OptionsBuilder.UseSqlServer("Server=LAPTOP-AT94SBBO\\SQLEXPRESS;Database=CrockswearDb;Trusted_Connection=True;TrustServerCertificate=True;");
+            }
+            base.OnConfiguring(OptionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Basket>().Property(o => o.Status).HasConversion<string>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Shoe> Shoes { get; set; }
+        public DbSet<Basket> Orders { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+
+    }
+}
